6ec1e4524eecb4e3df22e325bdb62eab5051d7fb
wali.rahman89@yahoo.com <wali.rahman89@yahoo.com>
2017-11-13 01:26:10 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,18 +2,22 @@
 const drivers = ["Milo", "Otis", "Garfield"] ;
 
 
-function destructivelyAppendDriver (name) {
+function destructivelyAppendDriver(name) {
   drivers.push(name);
 }
 
-function destructivelyPrependDriver (name) {
+function destructivelyPrependDriver(name) {
   drivers.unshift(name);
 }
 
-function destructivelyRemoveLastDriver () {
+function destructivelyRemoveLastDriver() {
   drivers.pop();
 }
 
-function destructivelyRemoveFirstDriver () {
+function destructivelyRemoveFirstDriver() {
   drivers.shift();
+}
+
+function appendDriver(name) {
+  
 }
