178a616b2f3b52bf13f129682d7d891b2913d29c
wali.rahman89@yahoo.com <wali.rahman89@yahoo.com>
2017-11-13 01:20:14 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,11 +3,11 @@ const drivers = ["Milo", "Otis", "Garfield"] ;
 
 
 function destructivelyAppendDriver(name) {
-  drivers.push (name);
+  drivers.push(name);
 }
 
 function destructivelyPrependDriver(name) {
-  drivers.unshift (name);
+  drivers.unshift(name);
 }
 
 function removeLastDriver() {
